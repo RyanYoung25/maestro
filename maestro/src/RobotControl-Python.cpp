88073c16a63536e77deee99c68068f1c70b8e489
isaacgaretmia <isaacgaretmia@gmail.com>
2013-06-24 12:56:11 -0400

    Slight namespace error. Let's try that again.

--- maestro/src/RobotControl-Python.cpp
+++ maestro/src/RobotControl-Python.cpp
@@ -5,18 +5,17 @@
 
 using namespace boost::python;
 
-
 struct NullDeleter{
 	void operator()( const void* ){};
 };
 
-shared_ptr<RobotControl> getSharedInstance(){
-	return shared_ptr<RobotControl>( RobotControl::getInstance(), NullDeleter() );
+boost::shared_ptr<RobotControl> getSharedInstance(){
+	return boost::shared_ptr<RobotControl>( RobotControl::getInstance(), NullDeleter() );
 }
 
 BOOST_PYTHON_MODULE(Control) {
 
-	class_< RobotControl, shared_ptr<RobotControl>, boost::noncopyable >("RobotControl", no_init)
+	class_< RobotControl, boost::shared_ptr<RobotControl>, boost::noncopyable >("RobotControl", no_init)
 			.def("updateState", &RobotControl::updateState)
 			.def("getInstance", &getSharedInstance, return_value_policy<reference_existing_object>())
 			.staticmethod("getInstance");
